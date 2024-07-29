93d65a346a83f0dc421350ca9413333c153bec66
Josh Gretz <jgretz@truefit.io>
2024-07-29 10:09:58 -0500

    complete through exercise 5

--- exercises/5-rsc-compiler/global-worker/index.tsx
+++ exercises/5-rsc-compiler/global-worker/index.tsx
@@ -1,23 +1,57 @@
+// ./global-worker/index.tsx
+
 import { renderToReadableStream } from "react-dom/server";
-import { App } from "../src/App.js";
+import { createFromFetch } from "react-server-dom-esm/client.browser";
+import { accepts } from "../../../utils/headers.js";
+
+// Injected in at build-time by esbuild
+declare const REGION_WORKER_URL: string;
 
 export default {
-	async fetch(request, env) {
-		const url = new URL(request.url);
+  async fetch(request: Request, env) {
+    const url = new URL(request.url);
 
-		if (url.pathname === "/") {
-			// If the request is for `/`, SSR the app
-			return new Response(
-				await renderToReadableStream(<App />, {
-					bootstrapModules: ["/index.js"],
-				}),
-				{
-					headers: { "Content-Type": "text/html" },
-				},
-			);
-		}
+    // If the request is for our app
+    if (url.pathname === "/") {
+      // Allowlist some headers
+      const contentType = request.headers.get("Content-Type");
+      const rscAction = request.headers.get("RSC-Action");
 
-		// Otherwise, serve the static assets like normal
-		return env.ASSETS.fetch(request);
-	},
+      const headers = new Headers();
+
+      if (contentType) {
+        headers.set("Content-Type", contentType);
+      }
+      if (rscAction) {
+        headers.set("RSC-Action", rscAction);
+      }
+
+      // And forward the request to the region Worker
+      const dataResponsePromise = fetch(REGION_WORKER_URL, {
+        method: request.method,
+        headers,
+        body: request.body,
+      });
+
+      // If we're looking for HTML
+      const accept = request.headers.get("Accept");
+      if (accepts(accept, "text/html")) {
+        // Transform the region Worker response into HTML
+        const root = await createFromFetch(dataResponsePromise, {
+          moduleBaseURL: "/_.._/src",
+        });
+        return new Response(await renderToReadableStream(root), {
+          headers: { "Content-Type": "text/html" },
+        });
+      } else {
+        const rscResponse = await dataResponsePromise;
+        return new Response(rscResponse.body, {
+          headers: { "Content-Type": "text/x-component" },
+        });
+      }
+    }
+
+    // Otherwise, serve the static assets like normal
+    return env.ASSETS.fetch(request);
+  },
 } as ExportedHandler<{ ASSETS: Fetcher }>;
--- exercises/5-rsc-compiler/src/App.tsx
+++ exercises/5-rsc-compiler/src/App.tsx
@@ -1,28 +1,32 @@
+// ./src/App.tsx
 "use server";
 
 import { Suspense } from "react";
+import { BrowserReact } from "../../../utils/BrowserReact.js";
 import { Counter } from "./Counter.js";
 import { Todos } from "./Todos.js";
 
 export function App() {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>5 | Server Actions</title>
-			</head>
-			<body>
-				<div>
-					<p>"Hello, world!" from server-side rendered React!</p>
-					<img src="/react-summit.svg" width="128" />
-					<Counter />
-					<Suspense fallback={<p>Loading...</p>}>
-						{/* @ts-expect-error Async Server Component */}
-						<Todos />
-					</Suspense>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>5 | RSC compiler</title>
+        <BrowserReact />
+      </head>
+      <body>
+        <div>
+          <p>"Hello, world!" from server-side rendered React!</p>
+          <img src="/react-summit.svg" width="128" />
+          <Counter />
+          <Suspense fallback={<p>Loading...</p>}>
+            {/* @ts-expect-error Async Server Component */}
+            <Todos />
+          </Suspense>
+        </div>
+        <script type="module" src="/src/index.js"></script>
+      </body>
+    </html>
+  );
 }
