9ec7de25bee2fec7768aa89b226c4ea5f132642b
Josh Gretz <jgretz@truefit.io>
2024-07-29 09:32:58 -0500

    finish through exercise 2

--- exercises/2-intro-to-workers/scripts/build.ts
+++ exercises/2-intro-to-workers/scripts/build.ts
@@ -2,18 +2,38 @@ import { build } from "esbuild";
 import { cp } from "fs/promises";
 
 async function buildClientSideRenderedReactApp() {
-	await build({
-		platform: "browser",
-		format: "esm",
-		entryPoints: ["./src/index.tsx"],
-		bundle: true,
-		outdir: "./dist-global",
-	});
+  await build({
+    platform: "browser",
+    format: "esm",
+    entryPoints: ["./src/index.tsx"],
+    bundle: true,
+    outdir: "./dist-global",
+  });
 }
 
 async function copyStaticAssets() {
-	await cp("./public", "./dist-global", { recursive: true });
+  await cp("./public", "./dist-global", { recursive: true });
+}
+
+async function buildGlobalWorker() {
+  await build({
+    entryPoints: ["./global-worker/index.tsx"],
+    format: "esm",
+    platform: "neutral",
+    conditions: [
+      "workerd", // The Cloudflare Workers runtime is called 'workerd'
+      "browser",
+    ],
+    mainFields: ["workerd", "module", "main", "browser"],
+    bundle: true,
+    splitting: true,
+    outdir: "./dist-global/_worker.js",
+    define: {
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    },
+  });
 }
 
 await copyStaticAssets();
 await buildClientSideRenderedReactApp();
+await buildGlobalWorker();
